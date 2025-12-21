ca5524c4d99a94949ae53c5a9a4ddb7d57b0dce5
Sudhanshu8252 <sudhanshuranjan9608@gmail.com>
2025-12-21 14:54:35 +0530

    Time: 2 ms (99.11%), Space: 47.2 MB (13.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,5 +1,53 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer,Integer> map = new HashMap <>();
+        for (int i = 0; i < nums.length; i++) {
+
+        //int curr = nums[i]; 
+        //curr + x = target 
+        //x = target - curr
+
+        int x = target - nums[i];
+        if(map.containsKey(x)){
+            return new int[]{map.get(x),i};
+        }
+
+         map.put(nums[i], i);
+
+    }
+    return new int[]{};
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+          BRUTE FORCE 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
         for(int i =0;i<nums.length;i++){
             for(int j= i+1 ;j<nums.length;j++){
                if( nums[i] + nums[j] == target ){
@@ -12,26 +60,12 @@ class Solution {
 
     }
 }
-
-
-/*class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            //int curr = nums[i];
-            //curr + x = target
-            //x = target - curr
-            int x = target - nums[i];
-            if (map.containsKey(x)) {
-                return new int[] { map.get(x), i };
-            }
-            map.put(nums[i], i);
-
-        }
-        return null;
-    }
-}
 */
+
+
+
+
+
 
 
 
