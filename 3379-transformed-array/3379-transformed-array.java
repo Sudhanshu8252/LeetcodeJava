2d283155b3b846e85b6164ba9baf8f78cb938888
Sudhanshu Ranjan <sudhanshuranjan9608@gmail.com>
2026-02-06 00:15:40 +0530

    Time: 1 ms (98.72%), Space: 47 MB (13.78%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.java
+++ 3379-transformed-array/3379-transformed-array.java
@@ -1,11 +1,13 @@
 class Solution {
     public int[] constructTransformedArray(int[] A) {
         int n = A.length;
-        int[] res = new int[n];
+        int[] result = new int[n];
+        for(int i = 0; i < n; i ++){
+            result[i] = A[((i + A[i]) % n + n) % n];
+        }
+        return result;  
 
-        for (int i = 0; i < n; i++)
-            res[i] = A[(((i + A[i]) % n) + n) % n];
-
-        return res;
+        
+       
     }
 }
