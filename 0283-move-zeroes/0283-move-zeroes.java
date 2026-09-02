c80e12bd534b305acd747b7d8f4e3ea48fdc9de6
Sudhanshu Ranjan <sudhanshuranjan9608@gmail.com>
2026-09-02 20:42:21 +0530

    Time: 2 ms (91.84%), Space: 48 MB (6.82%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,5 +1,46 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        int j = 0 ;
+
+        for(int i = 0; i < nums.length;i++){
+            if(nums[i] != 0){
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
+
+            }
+
+           
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+class Solution {
+    public void moveZeroes(int[] nums) {
     
         int j = 0;
 
@@ -18,3 +59,5 @@ class Solution {
         
     }
 }
+
+*/
